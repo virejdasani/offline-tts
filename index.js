78670d7d5bca8e1eacedb6a92eb7f7e801d87c3f
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-09-15 12:43:07 +0530

    Made code lighter

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const speak = (text, voice = 0, speed = 1, volume = 1, pitch = 1) => {
   let speech = window.speechSynthesis
   let utterance = new SpeechSynthesisUtterance(text)
-
   utterance.voice = speechSynthesis.getVoices()[voice];
   utterance.rate = speed
   utterance.pitch = pitch
   utterance.volume = volume
-  speech.cancel()
-  
+  speech.cancel() 
   return speech.speak(utterance)
 }
-
 module.exports = speak
