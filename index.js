40cddea86d11dc966704cd22cf7eb7adb9689f29
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-09-15 12:02:08 +0530

    PLEASE WORK

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-const speak = (text, voice = 0, speed = 1, volume = 1, pitch = 1) => {
+
+const TTS = (text, lang = 'id-ID', volume = 1, rate = 1, pitch = 1) => {
   let synthesis = window.speechSynthesis
-  let speech = new SpeechSynthesisUtterance(text);
+  let utterance = new SpeechSynthesisUtterance(text)
 
-  speech.voice = speechSynthesis.getVoices()[voice];
-  speech.rate = speed;
-  speech.volume = volume;
-  speech.pitch = pitch;
-
+  utterance.lang = lang
+  utterance.rate = rate
+  utterance.pitch = pitch
+  utterance.volume = volume
   synthesis.cancel()
+  
+  return synthesis.speak(utterance)
+}
 
-  return synthesis.speak(speech);
-};
-
-const pauseSpeech = () => {
-  return speechSynthesis.pause();
-};
-
-const resumeSpeech = () => {
-  return speechSynthesis.resume();
-};
-
-const stopSpeech = () => {
-  return speechSynthesis.cancel();
-};
-
-module.exports = speak;
-module.exports = pauseSpeech;
-module.exports = resumeSpeech;
-module.exports = stopSpeech;
+module.exports = TTS
